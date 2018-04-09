5a49acde2f14a3a32eac99fec2cfe9c94f557c58
mwq2020 <mwq2020@163.com>
2018-04-09 16:10:47 +0800

    fix

--- application/index/controller/Search.php
+++ application/index/controller/Search.php
@@ -7,9 +7,34 @@ class Search extends \think\Controller
 {
     public function index()
     {
+        $keyword = isset($_REQUEST['keyword']) ? trim($_REQUEST['keyword']) : '';
 
-        $video_list = Loader::model('Video')->where('status',1)->order('video_id','desc')->limit(3)->select();
-        $this->assign('video_list',$video_list);
+        $hot_keyword_list = Loader::model('SearchKeyword')->order('search_num','desc')->limit(6)->select();
+        $this->assign('hot_keyword_list',$hot_keyword_list);
+
+        $query = Loader::model('Video')->where('status',1);
+        if(!empty($keyword)){
+            $query = $query->where('title|title','like','%'.$keyword.'%');
+
+            //更新搜索关键字信息
+            $keyword_info = Loader::model('SearchKeyword')->where('keyword',$keyword)->find();
+            if($keyword_info){
+                $keyword_info->search_num += 1;
+                $keyword_info->update_time = time();
+                $keyword_info->save();
+            } else {
+                Loader::model('SearchKeyword')->insert(['keyword'=>$keyword,'search_num'=>1,'add_time'=>time(),'update_time'=>time()]);
+            }
+
+        }
+        //获取视频列表
+        $search_list = $query->order('view_num','desc')->limit(12)->select();
+        $this->assign('search_list',$search_list);
+
+        //热门推荐
+        $hot_recommand = Loader::model('Video')->where('status',1)->order('view_num','desc')->limit(5)->select();
+        $this->assign('hot_recommand',$hot_recommand);
+
         $this->assign('page_title','视频搜索');
         return $this->fetch('search/index');
     }
--- application/index/controller/Video.php
+++ application/index/controller/Video.php
@@ -20,7 +20,38 @@ class Video extends \think\Controller
         $recommand_list = Loader::model('Video')->limit(5)->select();
         $this->assign('recommand_list',$recommand_list);
         $this->assign('page_title','视频详情');
+
+        if($video_info){
+            $video_info->view_num += 1;
+            $video_info->save();
+        }
+
         return $this->fetch('video/info');
+    }
+
+    /**
+     * 视频添加喜欢
+     */
+    public function add_like()
+    {
+        $return_data = ['code'=>200,'data'=>['status' => 1],'msg'=>''];
+        try {
+            $video_id = isset($_REQUEST['video_id']) ? intval($_REQUEST['video_id']) : 0;
+            if(empty($video_id)){
+                throw new \Exception('入参错误');
+            }
+            $video_info = Loader::model('Video')->find($video_id);
+            if(empty($video_id)){
+                throw new \Exception('视频不存在');
+            }
+
+            $video_info->like_num += 1;
+            $video_info->save();
+        } catch (\Exception $e) {
+            $return_data['code'] = 500;
+            $return_data['msg'] = $e->getMessage();
+        }
+        exit(json_encode($return_data));
     }
 
 }
