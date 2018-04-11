5d1c84349e17dcfdc59eaf2a860ab7a09ce3f075
mwq2020 <mwq2020@163.com>
2018-04-11 18:24:08 +0800

    fix

--- application/manage/controller/Banner.php
+++ application/manage/controller/Banner.php
@@ -6,14 +6,8 @@ use think\Loader;
 class Banner extends \think\Controller
 {
 
-    //用户列表（筛选）
-    //详情
-    //禁用
-    //添加
-    //修改
-
     /**
-     * 用户列表
+     * banner列表
      * @return mixed
      */
     public function index()
@@ -37,7 +31,7 @@ class Banner extends \think\Controller
     }
 
     /**
-     * 添加活动页面
+     * 添加banner页面
      * @return mixed
      */
     public function add()
@@ -89,11 +83,13 @@ class Banner extends \think\Controller
 
 
     /**
-     * 添加活动页面
+     * 添加banner页面
      * @return mixed
      */
     public function edit()
     {
+        $banner_info = Loader::model('Banner')->find($_REQUEST['banner_id']);
+        $this->assign('banner_info', $banner_info);
         if(empty($_POST)){
             $this->assign('error_msg', '');
             $this->view->engine->layout('layout');
@@ -111,32 +107,56 @@ class Banner extends \think\Controller
                     throw new \Exception('图片保存失败【'.$file->getError().'】');
                 }
             }
-            if(empty($image_name)){
-                throw new \Exception('banenr图片不能为空');
-            }
+
             if(empty($_REQUEST['title'])){
                 throw new \Exception('banenr标题不能为空');
             }
 
-            $data = [];
-            $data['title']          = $_REQUEST['title'];
-            $data['url']            = $_REQUEST['url'];
-            $data['banner_img']     = $image_name ? '/static/image/banner/'.$image_name : '';
-            $data['status']         = intval($_REQUEST['status']);
-            $data['banner_type']    = $_REQUEST['banner_type'];
-            $data['remark']         = $_REQUEST['remark'];
-            $data['add_time']       = time();
-            $data['update_time']    = time();
-            $flag = Loader::model('Banner')->insert($data);
+            $banner_info->title = $_REQUEST['title'];
+            $banner_info->url = $_REQUEST['url'];
+            if($image_name){
+                $banner_info->banner_img = $image_name ? '/static/image/banner/'.$image_name : '';
+            }
+            $banner_info->status = $_REQUEST['status'];
+            $banner_info->banner_type = $_REQUEST['banner_type'];
+            $banner_info->remark = $_REQUEST['remark'];
+            $banner_info->update_time = time();
+            $flag = $banner_info->save();
+
             if(empty($flag)){
-                throw new Exception('banenr添加失败');
+                throw new Exception('banenr修改失败');
             }
         } catch (Exception $e) {
             $this->assign('error_msg', $e->getMessage());
             $this->view->engine->layout('layout');
-            return $this->fetch('banner/add');
+            return $this->fetch('banner/edit');
         }
         return $this->redirect('banner/index');
+    }
+
+    /**
+     * 修改banner的状态
+     * @return mixed|void
+     */
+    public function change_status()
+    {
+        try {
+            if(!isset($_REQUEST['status']) || empty($_REQUEST['banner_id'])){
+                throw new \Exception('入参错误');
+            }
+            $banner_info = Loader::model('Banner')->where('banner_id',$_REQUEST['banner_id'])->find();
+            if(empty($banner_info)){
+                throw new \Exception('角色不存在');
+            }
+            $banner_info->status = intval($_REQUEST['status']);
+            $banner_info->save();
+        } catch (\Exception $e){
+            $error_msg = $e->getMessage();
+            $this->assign('error_msg',$error_msg);
+            $this->view->engine->layout('layout');
+            return $this->fetch('banner/add');
+        }
+        return $this->redirect('/manage/banner/index');
     }
 
 }
--- application/manage/controller/Contract.php
+++ application/manage/controller/Contract.php
@@ -7,12 +7,6 @@ use think\Loader;
 class Contract extends \think\Controller
 {
 
-    //用户列表（筛选）
-    //详情
-    //禁用
-    //添加
-    //修改
-
     /**
      * 合同列表
      * @return mixed
@@ -35,6 +29,165 @@ class Contract extends \think\Controller
 
         $this->view->engine->layout('layout');
         return $this->fetch('contract/index');
+    }
+
+    /**
+     * 合同添加
+     * @return mixed
+     */
+    public function add()
+    {
+        $error_msg = '';
+        if(empty($_POST)){
+            $this->assign('error_msg',$error_msg);
+            $this->view->engine->layout('layout');
+            return $this->fetch('contract/add');
+        }
+
+        try {
+            if(empty($_REQUEST['title'])){
+                throw new \Exception('合同名称不能为空');
+            }
+            if(empty($_REQUEST['contract_start_time'])){
+                throw new \Exception('合同开始时间不能为空');
+            }
+            if(empty($_REQUEST['contract_end_time'])){
+                throw new \Exception('合同结束时间不能为空');
+            }
+            if(empty($_REQUEST['customer_name'])){
+                throw new \Exception('客户姓名不能为空');
+            }
+            if(empty($_REQUEST['order_amount'])){
+                throw new \Exception('合同金额不能为空');
+            }
+            if(empty($_REQUEST['contract_time'])){
+                throw new \Exception('合同签订时间不能为空');
+            }
+
+            $contract_img = '';
+            $file = request()->file('contract_img');
+            if($file){
+                $info = $file->move(ROOT_PATH . 'public' . DS . 'static'. DS . 'image/contract');
+                if($info){
+                    $contract_img =  '/static/image/contract/'.$info->getSaveName();
+                }else{
+                    throw new \Exception('图片保存失败【'.$file->getError().'】');
+                }
+            }
+
+            //检查账户名是否已经存在
+            $account_exist = Loader::model('Contract')->where('title',$_REQUEST['title'])->find();
+            if(!empty($account_exist)){
+                throw new \Exception('合同名称已存在，请更换');
+            }
+
+            $data = [];
+            $data['title']    = $_REQUEST['title'];
+            $data['contract_start_time']    = strtotime($_REQUEST['contract_start_time']);
+            $data['contract_end_time']      = strtotime($_REQUEST['contract_end_time']);
+            $data['customer_name']  = $_REQUEST['customer_name'];
+            $data['order_amount']   = $_REQUEST['order_amount'];
+            $data['contract_time']  = strtotime($_REQUEST['contract_time']);
+
+            $data['link_person']        = $_REQUEST['link_person'];
+            $data['link_person_phone']  = $_REQUEST['link_person_phone'];
+            $data['contract_img']       = $contract_img;
+            $data['remark']             = $_REQUEST['remark'];
+            $data['add_time']           = time();
+            $flag = Loader::model('Contract')->insert($data);
+            if(empty($flag)){
+                throw new \Exception('合同添加失败');
+            }
+        } catch (\Exception $e){
+            $error_msg = $e->getMessage();
+            $this->assign('error_msg',$error_msg);
+            $this->view->engine->layout('layout');
+            return $this->fetch('contract/add');
+        }
+        return $this->redirect('/manage/contract/index');
+    }
+
+    /**
+     * 管理员修改
+     * @return mixed
+     */
+    public function edit()
+    {
+        $error_msg = '';
+        $contract_info = Loader::model('Contract')->find($_REQUEST['contract_id']);
+        $this->assign('contract_info',$contract_info);
+        if(empty($_POST)){
+            $this->assign('error_msg',$error_msg);
+            $this->view->engine->layout('layout');
+            return $this->fetch('contract/edit');
+        }
+
+        try {
+            if(empty($_REQUEST['title'])){
+                throw new \Exception('合同名称不能为空');
+            }
+            if(empty($_REQUEST['contract_start_time'])){
+                throw new \Exception('合同开始时间不能为空');
+            }
+            if(empty($_REQUEST['contract_end_time'])){
+                throw new \Exception('合同结束时间不能为空');
+            }
+            if(empty($_REQUEST['customer_name'])){
+                throw new \Exception('客户姓名不能为空');
+            }
+            if(empty($_REQUEST['order_amount'])){
+                throw new \Exception('合同金额不能为空');
+            }
+            if(empty($_REQUEST['contract_time'])){
+                throw new \Exception('合同签订时间不能为空');
+            }
+
+            $contract_img = '';
+            $file = request()->file('contract_img');
+            if($file){
+                $info = $file->move(ROOT_PATH . 'public' . DS . 'static'. DS . 'image/contract');
+                if($info){
+                    $contract_img =  '/static/image/contract/'.$info->getSaveName();
+                }else{
+                    throw new \Exception('图片保存失败【'.$file->getError().'】');
+                }
+            }
+
+            //检查账户名是否已经存在
+            $map = [];
+            $map['contract_id'] = ['<>',$_REQUEST['contract_id']];
+            $map['title'] = $_REQUEST['title'];
+            $account_exist = Loader::model('Contract')->where($map)->find();
+            if(!empty($account_exist)){
+                throw new \Exception('合同名已存在，请更换');
+            }
+
+            $contract_info->title    = $_REQUEST['title'];
+            $contract_info->contract_start_time     = strtotime($_REQUEST['contract_start_time']);
+            $contract_info->contract_end_time       = strtotime($_REQUEST['contract_end_time']);
+
+            $contract_info->customer_name   = $_REQUEST['customer_name'];
+            $contract_info->order_amount    = intval($_REQUEST['order_amount']);
+            $contract_info->contract_time   = strtotime($_REQUEST['contract_time']);
+
+            $contract_info->link_person         = $_REQUEST['link_person'];
+            $contract_info->link_person_phone   = $_REQUEST['link_person_phone'];
+            $contract_info->remark              = $_REQUEST['remark'];
+            if(!empty($contract_img)){
+                $contract_info->contract_img = $contract_img;
+            }
+
+            $flag = $contract_info->save();
+            if(empty($flag)){
+                throw new \Exception('合同修改失败');
+            }
+        } catch (\Exception $e){
+            $error_msg = $e->getMessage();
+            $this->assign('error_msg',$error_msg);
+            $this->view->engine->layout('layout');
+            return $this->fetch('contract/edit');
+        }
+        return $this->redirect('/manage/contract/index');
     }
 
 }
