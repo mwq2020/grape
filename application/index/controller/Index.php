b02aa99d42f931033d9de8b20a0468fcbb4fd8c6
mwq2020 <mwq2020@163.com>
2018-04-20 16:01:50 +0800

    fix

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -36,3 +36,19 @@ class Index extends \think\Controller
        return $this->fetch('test',$view_data); 
     }
 }
+
+/*
+THINK_PATH 框架系统目录
+ROOT_PATH 框架应用根目录
+APP_PATH 应用目录（默认为application）
+CONF_PATH 配置目录（默认为APP_PATH）
+LIB_PATH 系统类库目录（默认为 THINK_PATH.'library/'）
+CORE_PATH 系统核心类库目录 （默认为 LIB_PATH.'think/'）
+TRAIT_PATH 系统trait目录（默认为 LIB_PATH.'traits/'）
+EXTEND_PATH 扩展类库目录（默认为 ROOT_PATH . 'extend/')
+VENDOR_PATH 第三方类库目录（默认为 ROOT_PATH . 'vendor/'）
+RUNTIME_PATH 应用运行时目录（默认为 ROOT_PATH.'runtime/'）
+LOG_PATH 应用日志目录 （默认为 RUNTIME_PATH.'log/'）
+CACHE_PATH 项目模板缓存目录（默认为 RUNTIME_PATH.'cache/'）
+TEMP_PATH 应用缓存目录（默认为 RUNTIME_PATH.'temp/'）
+*/
