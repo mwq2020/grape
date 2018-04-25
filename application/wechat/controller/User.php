70ef2654786e72e96c18024b7c6958fb1748b8d9
mwq2020 <mwq2020@163.com>
2018-04-25 15:26:44 +0800

    fix

--- application/wechat/controller/User.php
+++ application/wechat/controller/User.php
@@ -2,40 +2,249 @@
 namespace app\wechat\controller;
 use \think\Db;
 use think\Loader;
+use think\Session;
 
 class User extends \think\Controller
 {
     public function index()
     {
+        $user_id = session('user_id');
+        if(empty($user_id)){
+            $this->assign('page_title','紫葡萄少儿艺术库');
+            return $this->fetch('no_login_user_index');
+        }
+
         $this->assign('page_title','紫葡萄少儿艺术库');
         return $this->fetch('index');
     }
 
+    /**
+     * 用户登录
+     */
+    public function login()
+    {
+        $return_data = ['code' => 200,'data'=>[],'msg'=>''];
+        try {
+            //调用接口判断登录是否正确、
+            $reader_no = isset($_REQUEST['reader_no']) ? $_REQUEST['reader_no'] : '';
+            $password = isset($_REQUEST['password']) ? $_REQUEST['password'] : '';
 
+            $flag = Loader::model('User')->Login($reader_no,$password);
+            if(empty($flag)){
+                throw new \Exception('登录失败，请检查用户名及密码,');
+            }
+
+            $user_info = Loader::model('User')->where('reader_no',$reader_no)->find();
+            if(empty($user_info)){
+                $user_info = [];
+                $user_info['reader_no'] = $reader_no;
+                $user_info['status'] = 1;
+                $user_info['register_time'] = time();
+                $flag = Loader::model('User')->insert($user_info);
+                if(empty($flag)){
+                    throw new \Exception('登录失败，请检查用户名及密码.');
+                }
+                $user_info['user_id'] = Loader::model('User')->getLastInsID();
+            }
+            $return_data['data'] = ['user_id' => $user_info['user_id'],'reader_no' => $user_info['reader_no']];
+            //session('[start]');
+            session('user_id',$user_info['user_id']);
+            session('reader_no',$user_info['reader_no']);
+            if(!empty($user_info['reader_no'])){
+                session('real_name',$user_info['reader_no']);
+            } else {
+                session('real_name',$user_info['telphone']);
+            }
+            session('avatar','/static/image/user/default_user.jpg');
+            session('user_info',$user_info);
+        } catch (\Exception $e){
+            $return_data['code'] = 500;
+            $return_data['msg'] = $e->getMessage();
+        }
+        exit(json_encode($return_data));
+    }
+
+    /**
+     * 用户退出登录
+     */
+    public function logout()
+    {
+        Session::clear();
+        $this->redirect('/wechat/index/index');
+    }
+
+    /**
+     * 用户的观看视频记录
+     * @return mixed|void
+     */
     public function view_list()
     {
-        $this->assign('page_title','紫葡萄少儿艺术库');
+        $user_id = session('user_id');
+        if(empty($user_id)){
+            return $this->redirect('/?error=no_login');
+        }
+
+        $query = Db::table('user_view_list')->alias('a')->join('video b','a.video_id=b.video_id');
+
+        $type = isset($_REQUEST['type']) ? intval($_REQUEST['type']) : 0;
+        $type = in_array($type,[1,2,3]) ? $type : 0;
+        if($type == 1){
+            //$end_time = time();
+            $start_time = strtotime(date('y-m-d',strtotime('-7 days')));
+            $query = $query->where('a.date_time','>=',$start_time);
+        } elseif($type == 2) {
+            //$end_time = time();
+            $start_time = strtotime(date('y-m-d',strtotime('-30 days')));
+            $query = $query->where('a.date_time','>=',$start_time);
+        } elseif($type == 3) {
+            //$end_time = time();
+            $start_time = strtotime(date('y-m-d',strtotime('-180 days')));
+            $query = $query->where('a.date_time','>=',$start_time);
+        }
+        $view_list = $query->where('a.user_id',$user_id)->paginate(8,false,['query' => $_GET]);    //->select(); //todo 用户id 替换成正确的
+        $page = $view_list->render();
+        $this->assign('page', $page);
+//        echo "<pre>";
+//        print_r($_REQUEST);
+//        print_r($page);
+//        exit;
+        $this->assign('view_list',$view_list);
+        $this->assign('type',$type);
+        $this->assign('page_title','用户中心-历史浏览记录');
         return $this->fetch('view_list');
     }
 
-
+    /**
+     * 用户的消息列表
+     * @return mixed|void
+     */
     public function message_list()
     {
-        $this->assign('page_title','紫葡萄少儿艺术库');
+
+        $user_id = session('user_id');
+        if(empty($user_id)){
+            return $this->redirect('/?error=no_login');
+        }
+
+        $type = isset($_REQUEST['type']) ? intval($_REQUEST['type']) : 0;
+        $type = in_array($type,[1,2,3]) ? $type : 1;
+
+        $message_list = Db::table('message')->where('type',$type)->where('user_id',$user_id)->paginate(5,false,['query' => $_GET]);
+        $page = $message_list->render();
+        $this->assign('page', $page);
+//        echo "<pre>";
+//        print_r($message_list);
+//        exit;
+        $this->assign('message_list',$message_list);
+        $this->assign('type',$type);
+        $this->assign('page_title','用户中心-我的消息');
         return $this->fetch('message_list');
     }
 
-
+    /**
+     * 用户的作品列表
+     * @return mixed|void
+     */
     public function product_list()
     {
-        $this->assign('page_title','紫葡萄少儿艺术库');
+
+        $user_id = session('user_id');
+        if(empty($user_id)){
+            return $this->redirect('/?error=no_login');
+        }
+
+        $query = Db::table('product')->alias('a')->join('activity b','a.activity_id=b.activity_id');
+
+        $type = isset($_REQUEST['type']) ? intval($_REQUEST['type']) : 0;
+        $type = in_array($type,[1,2,3]) ? $type : 0;
+        if($type == 1){
+            $start_time = strtotime(date('y-m-d',strtotime('-7 days')));
+            $query = $query->where('a.add_time','>=',$start_time);
+        } elseif($type == 2) {
+            $start_time = strtotime(date('y-m-d',strtotime('-30 days')));
+            $query = $query->where('a.add_time','>=',$start_time);
+        } elseif($type == 3) {
+            $start_time = strtotime(date('y-m-d',strtotime('-180 days')));
+            $query = $query->where('a.add_time','>=',$start_time);
+        }
+        $product_list = $query->where('a.user_id',$user_id)->paginate(4,false,['query' => $_GET]);
+        $page = $product_list->render();
+        $this->assign('page', $page);
+
+//        echo "<pre>";
+//        print_r($product_list);
+//        print_r($page);
+//        exit;
+
+        $this->assign('product_list',$product_list);
+        $this->assign('type',$type);
+        $this->assign('page_title','用户中心-我的作品集');
         return $this->fetch('product_list');
     }
 
+    /**
+     * 用户的收藏列表
+     * @return mixed
+     */
     public function collect_list()
     {
         $this->assign('page_title','紫葡萄少儿艺术库');
         return $this->fetch('collect_list');
+    }
+
+
+    public function product_info()
+    {
+        $user_id = session('user_id');
+        if(empty($user_id)){
+            return $this->redirect('/?error=no_login');
+        }
+
+        $product_id = isset($_REQUEST['product_id']) ? intval($_REQUEST['product_id']) : 0;
+        $product_info = Loader::model('Product')->find($product_id);
+        if(empty($product_info)){
+            return $this->redirect('/?error=no_product');
+        }
+        $this->assign('product_info',$product_info);
+
+        $product_info->view_num += 1;
+        $product_info->save();
+
+        //我的其他作品
+        $product_list = Db::table('product')->where('user_id',$user_id)->limit(3)->select();
+        $this->assign('product_list',$product_list);
+
+        $this->assign('page_title','用户中心-我的作品详情');
+        return $this->fetch('productioninfo');
+    }
+
+
+    /**
+     * 更改message状态
+     */
+    public function change_message_status()
+    {
+        $return_data = ['code' => 200,'msg' => '','data'=>['product_id'=>0]];
+        try {
+            $message_id = isset($_REQUEST['message_id']) ? $_REQUEST['message_id'] : '';
+            if(empty($message_id)){
+                throw new \Exception('参数错误');
+            }
+            $message_info = Loader::model('Message')->find($message_id);
+            if(empty($message_info)){
+                throw new \Exception('消息不存在');
+            }
+
+            if($message_info['status'] != 1){
+                $message_info->status = 1;
+                $message_info->save();
+            }
+        } catch (\Exception $e){
+            $return_data['code']    = 500;
+            $return_data['msg']     = $e->getMessage();
+        }
+        exit(json_encode($return_data));
+
     }
 
 }
--- public/wechat/js/common.js
+++ public/wechat/js/common.js
@@ -19,4 +19,56 @@ $(function(){
         window.location = '/wechat/search/index?keyword='+$('#search_keywords').val();
     })
 
+    $('#login_btn').click(function(){
+        console.log('login click')
+        $('#login_pop').show();
+    });
+
+    $('.closed').click(function(){
+        console.log('login close click')
+        $(this).parent().parent().hide();
+    });
+
+
+    // 登录弹层提交时间
+    $('#login_confirm_btn').click(function(){
+        var reader_no = $('input[name=reader_no]').val();
+        var password = $('input[name=password]').val();
+        if(reader_no == ''){
+            return  $('#login_error').html('<span>!</span>读者证号不能为空！').show();
+        }
+        if(password == ''){
+            return  $('#login_error').html('<span>!</span>密码不能为空！').show();
+        }
+        $('#login_error').html('').hide();
+
+        console.log('登录账号',reader_no);
+        console.log('登录账号',password);
+
+        $.ajax({
+            type: 'POST',
+            async: false, //注意这里要求为Boolean类型的参数，false不能写成'false'不然会被解析成true
+            url: '/wechat/user/login' ,
+            data: 'reader_no='+ reader_no +'&password=' + password + '&m='+ Math.random() ,
+            dataType:'json',
+            cache:false, //同理
+            success: function(data){
+                if(data.code == 200){
+                    //window.location = '/index/index/index';
+                    location.reload();
+                } else {
+                    return $('#login_error').html('<span>!</span>'+data.msg).show();
+                }
+            } ,
+            error:function(){
+                return $('#login_error').html('<span>!</span>登录失败，请刷新重试').show();
+            },
+        });
+    })
+
+
+
+
+
+
 })
