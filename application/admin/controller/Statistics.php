e936f31f88a965623fd959646e9385d182e16529
mwq2020 <mwq2020@163.com>
2018-06-12 11:10:53 +0800

    fix

--- application/admin/controller/Statistics.php
+++ application/admin/controller/Statistics.php
@@ -16,4 +16,28 @@ class Statistics extends Base
         return $this->fetch('statistics/index');
     }
 
+
+    public function report_data()
+    {
+        $return_data = [
+            'labels'=>[1,2,3,4,5,6,7,8,9,10],
+            'data'=>[
+                [
+                    'data'=>[2,3,3,9,1,3,5,8,3,2],
+                    'markPoint' => ['data'=> null],
+                    'name'=>'独立ip',
+                    'type'=>'line'
+                ],
+                [
+                'data'=>[10,28,30,77,10,30,50,70,15,8],
+                'markPoint' => ['data'=> null],
+                'name'=>'访问量',
+                'type'=>'line'
+            ]
+            ]
+        ];
+
+        exit(json_encode($return_data));
+    }
+
 }
